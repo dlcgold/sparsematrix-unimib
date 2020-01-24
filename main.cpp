dd038986d623dd32dd352dbb9bf29354236042f7
dlcgold <dlcgold96@gmail.com>
2020-01-24 18:43:09 +0100

    continuo check memoria

--- main.cpp
+++ main.cpp
@@ -4,15 +4,22 @@
 typedef sparse_matrix<int> smatrixi;
 int main(){
   smatrixi test(0);
-  std::cout << test.get_default_value() << "\n";
-  test.add(3, 2, 2);
+  //std::cout << test.get_default_value() << "\n";
+   test.add(3, 2, 2);
   test.add(1, 1, 2);
   test.add(5, 0, 1);
   test.add(7, 1, 1);
   test.add(8, 1, 1);
+  test.add(90, 830, 10);
   test.add(100, 10, 80);
-  test.add(99, 99, 76);
+  test.add(99, 22, 76);
   test.add(1, 10, 20000);
-  std::cout << test.get_size() << "\n";
+  test.add(1, 30, 20000);
+  std::cout << "numero elementi: "<<  test.get_size() << "\n";
+  std::cout << "numero righe: "<<  test.get_size_row() << "\n";
+  std::cout << "numero colonne: "<<  test.get_size_column() << "\n";
+
   test.print();
+  test.clear();
+  return 0;
 }
--- sparsematrix.hpp
+++ sparsematrix.hpp
@@ -2,6 +2,8 @@
 #define SPARSEMATRIX_H
 
 #include <cmath>
+#include <iostream>
+
 template <typename T>
 class sparse_matrix{
 private:
@@ -22,22 +24,28 @@ private:
 						next(nullptr){}
   
    
-    //node(const T &v, node *n): value(v), next(n){}
-    
+    ~node(){
+      next = nullptr;
+    }    
   };
 
   node *head;
   unsigned int size;
 
-  float concat(int a, int b) { 
-    float len = floor(log10(b) + 1);
-    float corr = pow(10, len);
-    return a + b / corr;
-  } 
+  
+
+  void recursive_clear(node *n){
+    if(n!=nullptr) {
+      recursive_clear(n->next);	
+      delete n;
+      size--;
+      n = nullptr;	
+    }
+  }
 public:
 
-  sparse_matrix(T dvalue): default_value(dvalue), size(0), head(nullptr),
-			   size_row(0), size_column(0){};
+  sparse_matrix(T dvalue): default_value(dvalue),size_row(0), size_column(0),
+			   head(nullptr), size(0){};
   
 
   const T get_default_value() const {
@@ -57,47 +65,95 @@ public:
     return size;
   }
 
+  sparse_matrix &operator=(const sparse_matrix &other) {
+    
+    if(this != &other) {
+      sparse_matrix tmp(other);
+      std::swap(head,tmp.head);
+      std::swap(size,tmp.size);
+    }
+    return *this;
+  }
+
+  bool find(const T &v) const{
+    node *current = head;
+    while(current != nullptr){
+      if(current->value == v)
+	return true;
+      current = current->next;
+    }
+    return false;
+  }
+  
   void add(const T &v, const int i, const int j) {
     node *elem = new node(v, i, j);
-    node *temp = new node();
+    node *temp = head;
+    size++;
+    int max_row = 0;
+    int max_column = 0;
     if(head == nullptr){
       head = elem;
-      size++;
+      //size++;
+      elem = nullptr;
+      delete elem;
+      return;
     }else {
       // indice della testa maggiore quindi il nodo diventa la testa
       if((head->i == i && head->j > j) || (head->i > i)) {
 	elem->next = head;
 	head = elem;
-	size++;
+	elem = nullptr;
+	//size++;	  
       }else {
 	// nodo iteratore che cicla fino alla fine
 	// o fino a che sono sulla stessa riga ma ho trovato un valore
 	// di colonna maggiore o sono finito sulla stessa riga
-	temp = head;
+	
 	while(temp->next != nullptr &&
 	      ((temp->next->i == i && temp->next->j <= j)
 	       || (temp->next->i <= i))) {
 	  temp = temp->next;
 	}
+
 	// se sono arrivato alla fine aggiungo il valore alla fine
 	if(temp->next == nullptr) {
 	  elem->next = nullptr;
 	  temp->next = elem;
-	  size++;
+	 
+	  //size++;
 	}else {
 	  // aggiungo il valore in mezzo
 	  // se sono uguali tengo l'ultimo valore aggiunto
 	  if(elem->i == temp->i && elem->j == temp->j) {
 	    temp->value = elem->value;
+	    size--;
 	  }else {
 	    // altrimenti inserisco in mezzo
 	    elem->next = temp->next;
 	    temp->next = elem;
-	    size++;
+	    
+	    //size++;
 	  }
 	}
+	
       }
     }
+    // conto massima riga e colonna
+    node *iter = head;
+    while(iter != nullptr){
+      if(iter->i > max_row)
+	max_row = iter->i;
+      if(iter->j > max_column)
+	max_column = iter->j;
+      iter = iter->next;
+    }
+    size_row = max_row;
+    size_column = max_column;
+    iter = nullptr;
+    temp=nullptr;
+    
+    elem = nullptr;
+    return;
     
   }
 
@@ -107,10 +163,19 @@ public:
       std::cout << current->value << " in (" << current->i << ", " << current->j
 		<<")\n";
       current = current->next;
-	
+      
     }
   }
+
+  void clear(){
+    recursive_clear(head);
+    head = nullptr;
+    return;
+  }
   
+  ~sparse_matrix(){
+    clear();
+  }
 };
 
 

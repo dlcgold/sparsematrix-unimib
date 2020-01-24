d6fb815252a9479e5d0141afa8d5b703b834683d
dlcgold <dlcgold96@gmail.com>
2020-01-24 22:34:37 +0100

    sistemato add e inizio iterator

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "sparsematrix.hpp"
-#include <vector>
+
 typedef sparse_matrix<int> smatrixi;
 int main(){
   smatrixi test(0);
   //std::cout << test.get_default_value() << "\n";
-   test.add(3, 2, 2);
+  test.add(3, 2, 2);
   test.add(1, 1, 2);
   test.add(5, 0, 1);
   test.add(7, 1, 1);
@@ -15,11 +15,28 @@ int main(){
   test.add(99, 22, 76);
   test.add(1, 10, 20000);
   test.add(1, 30, 20000);
+  test.add(54, 1, 4);
+  test.add(42, 3, 1);
+  test.add(1, 0, 1);
+  test.add(67, 20, 0);
+  test.add(1, 0, 0);
+  test.add(2, 0, 2);
+  test.add(3, 0, 0);
   std::cout << "numero elementi: "<<  test.get_size() << "\n";
   std::cout << "numero righe: "<<  test.get_size_row() << "\n";
   std::cout << "numero colonne: "<<  test.get_size_column() << "\n";
+  std::cout << "esiste elemento in (830, 10)? " << test.find(830, 10) << "\n";
+  std::cout << "esiste elemento in (83, 100)? " << test.find(80, 100) << "\n";
 
+  smatrixi copy(test);
   test.print();
   test.clear();
+  std::cout << "numero elementi after clear: "<<  test.get_size() << "\n";
+  std::cout << "print di copy\n";
+  copy.print();
+  smatrixi::const_iterator i,ie;
+
+  for(i=test.begin(),ie=test.end(); i!=ie; ++i)
+    std::cout<<*i<<std::endl;
   return 0;
 }
--- sparsematrix.hpp
+++ sparsematrix.hpp
@@ -3,6 +3,8 @@
 
 #include <cmath>
 #include <iostream>
+#include <iterator>
+#include <ostream>
 
 template <typename T>
 class sparse_matrix{
@@ -23,7 +25,9 @@ private:
     node(const T &v, const int x, const int y): value(v), i(x), j(y),
 						next(nullptr){}
   
-   
+    node(const T &v, const int x, const int y, node *n): value(v), i(x),
+							 j(y), next(n){}
+
     ~node(){
       next = nullptr;
     }    
@@ -75,10 +79,10 @@ public:
     return *this;
   }
 
-  bool find(const T &v) const{
+  bool find(int i, int j) const{
     node *current = head;
     while(current != nullptr){
-      if(current->value == v)
+      if(current->i == i && current->j == j)
 	return true;
       current = current->next;
     }
@@ -94,16 +98,13 @@ public:
     if(head == nullptr){
       head = elem;
       //size++;
-      elem = nullptr;
-      delete elem;
       return;
     }else {
       // indice della testa maggiore quindi il nodo diventa la testa
       if((head->i == i && head->j > j) || (head->i > i)) {
 	elem->next = head;
 	head = elem;
-	elem = nullptr;
-	//size++;	  
+        	  
       }else {
 	// nodo iteratore che cicla fino alla fine
 	// o fino a che sono sulla stessa riga ma ho trovato un valore
@@ -111,7 +112,7 @@ public:
 	
 	while(temp->next != nullptr &&
 	      ((temp->next->i == i && temp->next->j <= j)
-	       || (temp->next->i <= i))) {
+	       || (temp->next->i < i))) {
 	  temp = temp->next;
 	}
 
@@ -119,7 +120,7 @@ public:
 	if(temp->next == nullptr) {
 	  elem->next = nullptr;
 	  temp->next = elem;
-	 
+	  
 	  //size++;
 	}else {
 	  // aggiungo il valore in mezzo
@@ -127,15 +128,14 @@ public:
 	  if(elem->i == temp->i && elem->j == temp->j) {
 	    temp->value = elem->value;
 	    size--;
+	    // avendo sovrascritto cancello elem
+	    delete elem;
 	  }else {
 	    // altrimenti inserisco in mezzo
 	    elem->next = temp->next;
 	    temp->next = elem;
-	    
-	    //size++;
 	  }
-	}
-	
+	}	
       }
     }
     // conto massima riga e colonna
@@ -148,11 +148,7 @@ public:
       iter = iter->next;
     }
     size_row = max_row;
-    size_column = max_column;
-    iter = nullptr;
-    temp=nullptr;
-    
-    elem = nullptr;
+    size_column = max_column;;
     return;
     
   }
@@ -176,7 +172,110 @@ public:
   ~sparse_matrix(){
     clear();
   }
+
+  //copy constructor from other sparse matrix
+  sparse_matrix(const sparse_matrix &other): default_value(other.default_value),
+					     size_row(0), size_column(0),
+					     head(nullptr), size(0) {
+    node *curr = other.head;
+
+    try {
+      while(curr != nullptr) {
+	add(curr->value, curr->i, curr->j);
+	curr = curr->next;
+      }		
+    }
+    catch(...) {
+      clear();
+      throw;
+    }
+  }
+
+  // iteratore
+
+  class const_iterator{
+  private:
+    const node *iter;
+    
+  public:
+    //costruttori
+    const_iterator() : iter(nullptr) {}
+    const_iterator(const const_iterator &other) : iter(other.iter) {}
+
+    // override assegnamento
+    const_iterator& operator=(const const_iterator &other) {
+      iter = other.iter;
+      return *this;
+    }
+
+    //distruttore
+    ~const_iterator() {}
+
+    // ovveride accesso
+    const T& operator*() const{
+      return iter->value;
+    }
+    const T& operator->() const{
+      return &(iter->value);
+    }
+
+    // override post e pre incremento
+    const_iterator operator++(int) {
+      const_iterator tmp(*this);
+      iter = iter->next;
+      return tmp;
+    }
+    
+    const_iterator& operator++() {
+      iter = iter->next;
+      return *this;
+    }
+
+    // override uguaglianza
+    bool operator==(const const_iterator &other) const {
+      return (iter == other.iter);
+    }
+		
+    /**
+       @brief operator !=
+       @return true if not equal
+    */
+    bool operator!=(const const_iterator &other) const {
+      return (iter != other.iter);
+    }
+
+  private:
+    friend class sparse_matrix;
+
+    // costruttore per iterare su struttura dati
+    const_iterator(const node *iter) : iter(iter) {}
+  };
+
+  // begin e end
+  const_iterator begin() const {
+    return const_iterator(head);
+  }
+  const_iterator end() const {
+    return const_iterator(nullptr);
+  }
 };
 
+// overload << per stampa
+template <typename T>
+std::ostream &operator<<(std::ostream &os, 
+			 const sparse_matrix<T> smatrix) {
+	
+  typename sparse_matrix<T>::const_iterator i, ie;
+	
+  i = smatrix.begin();
+  ie = smatrix.end();
 
+  while(i!=ie) {
+    os << *i << " ";
+    
+    ++i;
+  }
+
+  return os;
+}
 #endif
